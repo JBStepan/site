525e429ee431468a9110c354eb301e9cd575a0a2
jbstepan <29986350+JBStepan@users.noreply.github.com>
2025-02-26 11:34:13 -0600

    Fixed blogpost formatting

---
+++ src/routes/blog/[slug]/Tag.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { tag }: { tag: string } = $props() 
+</script>
+
+<span class=" bg-gray-500 opacity-75 text-white rounded-full mx-1 px-1 text-center">
+    #{tag}
+</span>
